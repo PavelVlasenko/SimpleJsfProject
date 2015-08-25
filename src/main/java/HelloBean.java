51f4b8cb1eb4b117a5af588322d22d10c2affa9b
PavelVlasenko <vlasenko-pav@yandex.ru>
2015-08-26 00:15:03 +0700

    Initial commit

---
+++ src/main/java/HelloBean.java
@@ -0,0 +1,33 @@
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+/**
+ * Created by pavel on 25.08.15.
+ */
+@ManagedBean
+@SessionScoped
+public class HelloBean
+{
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void buttonAction(ActionEvent actionEvent) {
+        addMessage("Welcome to Primefaces!!");
+    }
+
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
